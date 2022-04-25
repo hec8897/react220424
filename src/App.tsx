fe69a4f5a41d5fd4780f21d711380255e8652427
dawoon <hec8897@naver.com>
2022-04-25 22:14:35 +0900

    수정 삭제 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { useState,useRef } from "react";
+import { useState, useRef } from "react";
 import "./App.css";
 import TodoList from "./components/TodoList";
 import TodoCreate from "./components/TodoCreate";
 
 function App() {
-  const nextId = useRef(1)
+  const nextId = useRef(1);
   const [lists, setLists] = useState([
     {
       id: 0,
       name: "dawoon",
       desc: "할일",
+      active: false,
     },
   ]);
 
@@ -17,20 +18,35 @@ function App() {
   const [desc, setDesc] = useState("");
 
   const onChange = () => {
-    console.log(name,desc)
-    setLists([...lists, { id: nextId.current, name, desc }]);
+    setLists([...lists, { id: nextId.current, name, desc, active: false }]);
     nextId.current += 1;
   };
-  
+
   const onChangeName = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value, name } = e.target;
     name === "name" ? setName(value) : setDesc(value);
   };
 
+  const onActive = (id: number) => {
+    setLists(
+      lists.map((ele) => {
+        return ele.id === id ? { ...ele, active: !ele.active } : ele;
+      })
+    );
+  };
+
+  const onRemove = (id:number) =>{
+    setLists(
+      lists.filter((ele)=>{
+        return ele.id !== id
+      })
+    )
+  }
+
   return (
     <div className="App">
-      <TodoCreate onChange={onChange} onChangeName={onChangeName}/>
-      <TodoList TodoList={lists} />
+      <TodoCreate onChange={onChange} onChangeName={onChangeName} />
+      <TodoList TodoList={lists} onActive={onActive} onRemove={onRemove}/>
     </div>
   );
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,22 +1,31 @@
 interface listType {
-    id:number;
-    name:string;
-    desc:string;
+  id: number;
+  name: string;
+  desc: string;
+  active: boolean;
 }
 
-interface props{
-    TodoList:Array<listType>
+interface props {
+  TodoList: Array<listType>;
+  onActive: (id: number) => void;
+  onRemove: (id: number) => void;
 }
 
-function TodoList({TodoList}:props){
-    return(
-        <div>
-            <ul>
-                {TodoList.map((ele)=><div key={ele.id}>{ele.name}</div>)}
-            </ul>
-        </div>
-    );
-
+function TodoList({ TodoList, onActive, onRemove }: props) {
+  return (
+    <div>
+      <ul>
+        {TodoList.map((ele) => (
+          <li key={ele.id} style={{ color: ele.active ? "red" : "#222" }}>
+            <div>
+              <b onClick={() => onActive(ele.id)}>{ele.name}</b> : {ele.desc}
+              <button onClick={() => onRemove(ele.id)}>삭제</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default TodoList;
